71b4750128d4b12fd3ea0bb43d75f7bdc15575d6
Alex Spirin <15005465+papasasha42@users.noreply.github.com>
2018-07-25 18:31:31 +0300

    basic functionality ready

--- starter/app.js
+++ starter/app.js
@@ -8,3 +8,86 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+const init = () => {
+  gamePlaying = true;
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  diceDOM = document.querySelector('.dice');
+  btnRoll = document.querySelector('.btn-roll');
+  btnHold = document.querySelector('.btn-hold');
+
+  btnRoll.style.display = 'block';
+  btnHold.style.display = 'block';
+
+  document.querySelector('.dice').style.display = 'none';
+  document.querySelector('#score-0').textContent = '0';
+  document.querySelector('#score-1').textContent = '0';
+  document.querySelector('#current-0').textContent = '0';
+  document.querySelector('#current-1').textContent = '0';
+  document.querySelector('#name-0').textContent = 'Player 1';
+  document.querySelector('#name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.add('active');
+};
+
+let scores, roundScore, activePlayer, gamePlaying, diceDOM, btnRoll, btnHold;
+init();
+
+const nextPlayer = () => {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+  document.getElementById('current-0').textContent = 0;
+  document.getElementById('current-1').textContent = 0;
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  // diceDOM.style.display = 'none';
+};
+
+// button ROLL functionality
+btnRoll.addEventListener('click', () => {
+  if (gamePlaying) {
+    // get random number
+    const dice = Math.floor(Math.random() * 6) + 1;
+
+    // display the result
+    diceDOM.src = 'dice-' + dice + '.png';
+    diceDOM.style.display = 'block';
+
+    // update the round score IF rolled number wasn't 1
+    if (dice !== 1) {
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+      nextPlayer();
+    }
+  }
+});
+
+// button HOLD functionality
+btnHold.addEventListener('click', () => {
+  if (gamePlaying) {
+    // add current score to global score
+    scores[activePlayer] += roundScore;
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // check if player won the game
+    if (scores[activePlayer] >= 20) {
+      document.querySelector('#name-' + activePlayer).textContent = 'WINNER';
+      diceDOM.style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      gamePlaying = false;
+      btnRoll.style.display = 'none';
+      btnHold.style.display = 'none';
+    } else {
+      nextPlayer();
+    }
+  }
+});
+
+document.querySelector('.btn-new').addEventListener('click', init);
